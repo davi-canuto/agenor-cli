cee155f23b25d809ee7bc1dd6d814431b96a5158
davi-canuto <davicanutogregorio@gmail.com>
2023-09-04 20:48:02 -0300

    improvement: enhance init command

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,46 +1,35 @@
-import { Command } from '@oclif/core'
+import buildYaml from '../helpers/buildYaml'
+import { Command, Flags } from '@oclif/core'
 import * as yaml from 'js-yaml'
 import * as fs from 'fs'
-
+import { exec } from 'child_process'
 
 export default class Init extends Command {
   static description = 'Init YML form to create portifolio.'
 
+  static flags = {
+    code: Flags.boolean({
+      description: 'Open YAML file with Visual Studio Code',
+      required: false
+    }),
+  }
+
   async run(): Promise<void> {
-    const yamlData = {}
+    const yamlData = buildYaml()
+    const {flags} = await this.parse(Init)
 
     const yamlString = yaml.dump(yamlData)
 
     fs.writeFileSync('./index.yml', yamlString, 'utf8')
 
-    this.log('Your yml is it`s at your ./index.yml.')
+    if (flags.code) {
+      exec('code ./index.yml', (error) => {
+        if (error) {
+          this.log('Error in open with code, Your yml is it`s at your ./index.yml.');
+        }
+      });
+    } else {
+      this.log('Your yml is it`s at your ./index.yml.')
+    }
   }
 }
-
-
-// import {Args, Command, Flags} from '@oclif/core'
-
-// export default class Hello extends Command {
-//   static description = 'Say hello'
-
-//   static examples = [
-//     `$ oex hello friend --from oclif
-// hello friend from oclif! (./src/commands/hello/index.ts)
-// `,
-//   ]
-
-//   static flags = {
-//     from: Flags.string({char: 'f', description: 'Who is saying hello', required: true}),
-//   }
-
-//   static args = {
-//     person: Args.string({description: 'Person to say hello to', required: true}),
-//   }
-
-//   async run(): Promise<void> {
-//     const {args, flags} = await this.parse(Hello)
-
-//     this.log(`hello ${args.person} from ${flags.from}! (./src/commands/hello/index.ts)`)
-//   }
-// }
-
