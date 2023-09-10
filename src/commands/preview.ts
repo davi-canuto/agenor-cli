8d5b2b51fd37e0d82d03841d6b7d2530f3e0de8f
davi-canuto <davicanutogregorio@gmail.com>
2023-09-10 11:02:56 -0300

    feat: add connect to web api and works in enhance preview command

--- src/commands/preview.ts
+++ src/commands/preview.ts
@@ -1,6 +1,7 @@
 import { Command, Flags } from '@oclif/core'
 import { exec } from 'node:child_process'
 import getYamlJson from '../helpers/get-yaml-json'
+import Api from '../lib/api'
 export default class Preview extends Command {
   static description = 'Preview portofilio by your YML.'
 
@@ -10,5 +11,15 @@ export default class Preview extends Command {
     const { flags } = await this.parse(Preview)
 
     const yamlData = getYamlJson()
+
+    try {
+      const response = await Api.post('/api/portifolio', yamlData)
+
+      console.log(response)
+      // this.portifolioId = response.id
+    } catch (error) {
+      console.log(error)
+      throw new Error('error in create portifolio')
+    }
   }
 }
---
+++ src/lib/api.ts
@@ -0,0 +1,75 @@
+import axios, { AxiosInstance, AxiosResponse } from 'axios'
+
+export default class Api {
+  private static BASE_URL = 'http://localhost:3003'
+  private static instance: AxiosInstance
+
+  private static createInstance(): AxiosInstance {
+    if (!this.instance) {
+      this.instance = axios.create({
+        baseURL: this.BASE_URL,
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
+    }
+
+    return this.instance
+  }
+
+  static async get<T>(uri: string): Promise<T> {
+    try {
+      const response: AxiosResponse<T> = await this.createInstance().get(uri)
+      return this.respondWith<T>(response)
+    } catch (error) {
+      console.log(error)
+      throw new Error('error in request')
+    }
+  }
+
+  static async post<T>(uri: string, data: any): Promise<T> {
+    try {
+      const response: AxiosResponse<T> = await this.createInstance().post(
+        uri,
+        data,
+      )
+      return this.respondWith<T>(response)
+    } catch (error) {
+      console.log(error)
+      throw new Error('error in request')
+    }
+  }
+
+  static async put<T>(uri: string, data: any): Promise<T> {
+    try {
+      const response: AxiosResponse<T> = await this.createInstance().put(
+        uri,
+        data,
+      )
+      return this.respondWith<T>(response)
+    } catch (error) {
+      console.log(error)
+      throw new Error('error in request')
+    }
+  }
+
+  static async delete<T>(uri: string): Promise<T> {
+    try {
+      const response: AxiosResponse<T> = await this.createInstance().delete(uri)
+      return this.respondWith<T>(response)
+    } catch (error) {
+      console.log(error)
+      throw new Error('error in request')
+    }
+  }
+
+  private static respondWith<T>(response: AxiosResponse<T>): T {
+    const data: T = response.data
+    if (data && (data as any).errors) {
+      throw new Error((data as any).errors)
+    }
+
+    return data
+  }
+}
