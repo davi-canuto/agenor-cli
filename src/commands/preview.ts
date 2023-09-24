cec60845de03cb231b1490d9b089192e105c9acd
davi-canuto <davicanutogregorio@gmail.com>
2023-09-24 10:23:23 -0300

    refact: preview command

--- src/commands/preview.ts
+++ src/commands/preview.ts
@@ -4,6 +4,7 @@ import getBuffer from '../helpers/get-buffer'
 import getUrl from '../helpers/get-url'
 import * as fs from 'node:fs'
 import * as path from 'node:path'
+
 export default class Preview extends Command {
   static description = 'View your portfolio by current JSON.'
 
@@ -11,57 +12,58 @@ export default class Preview extends Command {
     try {
       const jsonData = fs.readFileSync('./form.json', 'utf8')
       const existingBuffer = getBuffer(true)
-
       let response
 
       if (existingBuffer) {
         const decryptedId = Buffer.from(existingBuffer, 'base64').toString(
           'utf8',
         )
-
-        response = await Api.put(
-          `/api/portifolio/${decryptedId}?preview=true`,
-          JSON.parse(jsonData),
-        ).then((res: any) => res)
-
-        if (!response.success) {
-          throw new Error('error in request')
-        }
-
-        const portifolioUrl = getUrl(response?.data?._id, true)
-
-        this.log('Your preview is successfully updated.')
-        this.log(`See results in ${portifolioUrl}`)
+        response = await this.updatePortfolioPreview(decryptedId, jsonData)
       } else {
-        response = await Api.post(
-          '/api/portifolio?preview=true',
-          JSON.parse(jsonData),
-        ).then((res: any) => res)
-
-        if (!response.success) {
-          throw new Error('error in request')
-        }
-
-        const data = response.data
-
-        const portifolioUrl = getUrl(data._id, true)
-
-        const currentDirectory = process.cwd()
-
-        const idTxtPath = path.join(currentDirectory, 'preview-id.txt')
-
-        fs.writeFileSync(
-          idTxtPath,
-          Buffer.from(data._id).toString('base64'),
-          'utf8',
-        )
-
-        this.log('Success in create your preview.')
-        this.log(`See results in ${portifolioUrl}`)
+        response = await this.createPortfolioPreview(jsonData)
       }
+
+      const portfolioUrl = getUrl(response.data?._id, true)
+      this.log('Your preview is successfully updated/created.')
+      this.log(`See results in ${portfolioUrl}`)
     } catch (error) {
-      console.log(error)
-      throw new Error('error in create portifolio')
+      console.error(error)
+      throw new Error('Error in creating/updating portfolio')
     }
+  }
+
+  private async updatePortfolioPreview(
+    decryptedId: string,
+    jsonData: string,
+  ): Promise<any> {
+    const response: any = await Api.put(
+      `/api/portifolio/${decryptedId}?preview=true`,
+      JSON.parse(jsonData),
+    )
+    if (!response.success) {
+      throw new Error('Error in update request')
+    }
+
+    return response
+  }
+
+  private async createPortfolioPreview(jsonData: string): Promise<any> {
+    const response: any = await Api.post(
+      '/api/portifolio?preview=true',
+      JSON.parse(jsonData),
+    )
+    if (!response.success) {
+      throw new Error('Error in create request')
+    }
+
+    const data = response.data
+    const currentDirectory = process.cwd()
+    const idTxtPath = path.join(currentDirectory, 'preview-id.txt')
+    fs.writeFileSync(
+      idTxtPath,
+      Buffer.from(data._id).toString('base64'),
+      'utf8',
+    )
+    return response
   }
 }
